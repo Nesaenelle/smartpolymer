e330319c3c732b8d5f6557c24bb49f1cd25b74db
Bogdan Zinkevych <venomsunset@gmail.com>
2019-09-06 14:36:56 +0300

    fix

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp = require("gulp"),
+    sass = require("gulp-sass"),
+    sourcemaps = require("gulp-sourcemaps");
+
+// ... other includes
+var browserSync = require("browser-sync").create();
+// ...
+
+function style() {
+    return (
+        gulp
+            .src('scss/main.scss')
+            // .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            // .pipe(postcss([autoprefixer(), cssnano()]))
+            // .pipe(rename('styles.css'))
+            // .pipe(sourcemaps.write())
+            .pipe(gulp.dest('public'))
+            // Add browsersync stream pipe after compilation
+            .pipe(browserSync.stream())
+    );
+}
+
+// A simple task to reload the page
+function reload() {
+    browserSync.reload();
+}
+
+function watch() {
+    style();
+    reload();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('scss/main.scss', style);
+    
+    gulp.watch('index.html', reload);
+}
+
+exports.watch = watch
---
+++ js/app.js
@@ -0,0 +1,55 @@
+$('.form').on('submit', (e) => {
+    e.preventDefault();
+
+    var controls = [...e.target.elements].filter(r => r.localName === 'input');
+    
+    if (e.target.checkValidity()) {
+        // var modalId = $(e.target).data('modal-id');
+        e.target.reset();
+  
+        // openModal(modalId);
+        e.target.classList.add('submitted');
+        e.target.querySelector('.btn').innerText = 'ОТПРАВЛЕНО';
+        controls.forEach(c=> {
+            resetValidaty(c);
+        });
+    } else {
+        var targetInput = controls.filter(r=> !r.classList.contains('success') || r.classList.contains('error'))[0];
+        if(targetInput) {
+            checkValidity(targetInput);
+            targetInput.focus();
+            // targetInput.scrollIntoView({block: "center", behavior: "smooth"});
+        }
+    }
+});
+
+$('.form').on('input', function(e) {
+    // checkValidity(e.target);
+    var controls = [...$(e.target).closest('form')[0].elements].filter(r => r.localName === 'input');
+    controls.forEach(c=> {
+        checkValidity(c);
+    });
+    // var targetInput = controls.filter(r=> !r.classList.contains('success') || r.classList.contains('error'))[0];
+    
+    // if(targetInput) {
+    //     $('.payment__step').eq(1).removeClass('active');
+    // } else {
+    //     $('.payment__step').eq(1).addClass('active');
+    // }
+});
+
+function checkValidity(el) {
+    if (el.checkValidity()) {
+        $(el).addClass('success');
+        $(el).removeClass('error');
+    } else {
+        $(el).addClass('error');
+        $(el).removeClass('success');
+    }
+}
+
+function resetValidaty(el) {
+    $(el).removeClass('success');
+    $(el).removeClass('error');
+    // $('.payment__step').eq(1).removeClass('active');
+}
